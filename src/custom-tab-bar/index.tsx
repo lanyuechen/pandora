919d0fef6c32d2bacc6afdb21217b471d1730ff5
zhenglipeng <zhenglipeng@sensetime.com>
2020-09-04 19:07:32 +0800

    解决tabbar图标不居中bug

--- src/custom-tab-bar/index.tsx
+++ src/custom-tab-bar/index.tsx
@@ -1,15 +1,20 @@
-import React, { useMemo } from 'react';
+import React, { useMemo, useState } from 'react';
 import Taro from '@tarojs/taro';
 import { AtTabBar } from 'taro-ui';
 
-export default () => {
+import './index.scss';
+
+const T = () => {
   const tabList = useMemo(() => [
     { title: '项目', iconType: 'bookmark', url: '/pages/projects/index'},
     { title: '视图', iconType: 'iphone', url: '/pages/views/index'},
     { title: '组件', iconType: 'money', url: '/pages/components/index'},
   ], []);
 
+  const [ current, setCurrent ] = useState(0);
+
   const handleClick = (idx: number) => {
+    setCurrent(idx);
     Taro.switchTab({
       url: tabList[idx].url
     });
@@ -19,7 +24,13 @@ export default () => {
     <AtTabBar
       tabList={tabList}
       onClick={handleClick}
-      current={0}
+      current={current}
     />
+  )
+}
+
+export default () => {
+  return (
+    <T />
   )
 }
