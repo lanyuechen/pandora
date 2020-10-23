42563d22832eced97c0fd4de5441e29cb1f2242b
zhenglipeng <zhenglipeng@sensetime.com>
2020-10-23 11:51:11 +0800

    添加组件入口

--- src/wardrobe/index.ts
+++ src/wardrobe/index.ts
@@ -3,6 +3,7 @@ import Paragraph from './paragraph';
 import Header from './header';
 import Image from './image';
 import Tadpole from './tadpole';
+import LoanCalc from './loan-calc';
 
 export default {
   Swiper,       // 轮播图
@@ -10,4 +11,5 @@ export default {
   Header,       // 标题
   Image,        // 图片
   Tadpole,      // 蝌蚪
+  LoanCalc,     // 贷款计算器
 }
